9611f949032b2898c0fe983be0e2e98efd9fd9d6
Alexandr Kolenchenko <sasha_2014@mail.ru>
2021-07-18 09:35:43 +0300

    .vscode/targets.log

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,11 @@
+
+
+
+def game_is_even_number():
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    # написать функцию random_number
+    print('Question: ' + 'random_number')
+    # написать функцию users_answer
+    print('Your answer: ' + 'users_answer')
+    
+    print("'yes' is wrong answer ;(. Correct answer was 'no'. \nLet's try again, " + 'gamers_name' + '!')
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,11 @@
 #!/usr/local/bin/env python
 from brain_games.cli import welcome_user
-
+from brain_games.scripts.brain_even import game_is_even_number
 
 def main():
     welcome_user()
+    game_is_even_number()
+
 
 
 if __name__ == '__main__':
