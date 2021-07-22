56b9a2d8f3afb1cec61d3de833c7f4027589b24e
Alexandr Kolenchenko <sasha_2014@mail.ru>
2021-07-22 20:25:56 +0300

    brain_games/scripts/__pycache__/brain_games.cpython-39.pyc

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,9 @@
 #!/usr/local/bin/env python
-from brain_games.cli import welcome_user
+#from brain_games.cli import welcome_user
 from brain_games.scripts.brain_even import game_is_even_number
 
 def main():
-    welcome_user()
+    #welcome_user()
     game_is_even_number()
 
 
