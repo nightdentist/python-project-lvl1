8ea1e56006ffd531ba6bcf7fcce40ebeb3afe053
Alexander Kolenchenko <sasha_2014@mail.ru>
2023-09-07 09:10:09 +0300

    correction games4

--- brain_games/games/brain_gcd.py
+++ brain_games/games/brain_gcd.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-from math import gcd
 from random import randint
 
 from brain_games.games.play_game import play_game
@@ -10,8 +9,13 @@ RULES = 'Find the greatest common divisor of given numbers.'
 def get_next_question_and_correct_answer():
     random_number1 = randint(1, 100)
     random_number2 = randint(1, 100)
-    correct_answer = gcd(random_number1, random_number2)
     question = str(random_number1) + ' ' + str(random_number2)
+    while random_number1 != 0 and random_number2 != 0:
+        if random_number1 > random_number2:
+            random_number1 = random_number1 % random_number2
+        else:
+            random_number2 = random_number2 % random_number1
+    correct_answer = random_number1 + random_number2
     return question, correct_answer
 
 
