7c8649472b321067fb689421c4271db3effe2c15
Alexander Kolenchenko <sasha_2014@mail.ru>
2023-09-11 13:52:24 +0300

    renamed magic numbers2

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -3,10 +3,12 @@ from random import randint
 from brain_games.games.play_game import play_game
 
 RULES = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+MIN_QUESTION_INT = 2
+MAX_QUESTION_INT = 50
 
 
 def get_next_question_and_correct_answer():
-    question = randint(2, 50)
+    question = randint(MIN_QUESTION_INT, MAX_QUESTION_INT)
     divider = 2
     while question % divider != 0:
         divider += 1
--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -3,14 +3,21 @@ from random import randint
 from brain_games.games.play_game import play_game
 
 RULES = 'What number is missing in the progression?'
+MIN_PROGRESSION_INT = 1
+MAX_PROGRESSION_INT = 5
+INTERVAL_START = 2
+INTERVAL_FINISH = 4
+UNKNOW_START = 1
+UNKNOW_FINISH = 3
 
 
 def get_next_question_and_correct_answer():
-    start_prog = randint(1, 5)
-    fihish_prog = 25
-    interval_progression = randint(2, 4)
-    progression = list(range(start_prog, fihish_prog, interval_progression))
-    unknown_number = randint(1, 3)
+    start_progprogression = randint(MIN_PROGRESSION_INT, MAX_PROGRESSION_INT)
+    fihish_progression = 25
+    interval_progression = randint(INTERVAL_START, INTERVAL_FINISH)
+    progression = list(range(start_progprogression, fihish_progression,
+                             interval_progression))
+    unknown_number = randint(UNKNOW_START, UNKNOW_FINISH)
     correct_answer = progression[unknown_number]
     progression[unknown_number] = '..'
     question = " ".join(map(str, progression))
